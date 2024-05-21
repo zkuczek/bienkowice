fef0cd7c99724ced4958c5e461196c779564c8df
zkuczek <160599377+zkuczek@users.noreply.github.com>
2024-05-21 08:31:11 +0200

    Create 1podstawy.php

---
+++ php/1podstawy.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="pl">
+	<head>
+		<meta charset="utf-8">
+		<title>Pierwsza strona</title>
+		<meta name="description" content="Oto moja pierwsza strona na zajęciach">
+		<meta name="keywords" content="szkoła, lekcje, temat, nauka, html">
+		<meta name="author" content="Karol Kubiczek">
+	</head>
+<body>
+<?php
+// Skrypt ten wypisze tekst, uŻywając funkcji "print"
+print("PHP");
+?>
+</body>
+</html>
